de84e480c2ffe4a355186f2f7ad435cbf4150f28
Onur <onurdogruiletisim@gmail.com>
2023-01-03 22:46:39 +0300

    Add project files.

---
+++ SerialPortChat/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace SerialPortChat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboBoxPort = new System.Windows.Forms.ComboBox();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.buttonDisconnect = new System.Windows.Forms.Button();
+            this.textBoxReceived = new System.Windows.Forms.TextBox();
+            this.textBoxSend = new System.Windows.Forms.TextBox();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBoxPort
+            // 
+            this.comboBoxPort.FormattingEnabled = true;
+            this.comboBoxPort.Location = new System.Drawing.Point(236, 43);
+            this.comboBoxPort.Name = "comboBoxPort";
+            this.comboBoxPort.Size = new System.Drawing.Size(232, 21);
+            this.comboBoxPort.TabIndex = 0;
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.BackColor = System.Drawing.Color.GreenYellow;
+            this.buttonConnect.Location = new System.Drawing.Point(286, 99);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(131, 39);
+            this.buttonConnect.TabIndex = 1;
+            this.buttonConnect.Text = "Bağlan";
+            this.buttonConnect.UseVisualStyleBackColor = false;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // buttonDisconnect
+            // 
+            this.buttonDisconnect.BackColor = System.Drawing.Color.GreenYellow;
+            this.buttonDisconnect.Location = new System.Drawing.Point(286, 164);
+            this.buttonDisconnect.Name = "buttonDisconnect";
+            this.buttonDisconnect.Size = new System.Drawing.Size(131, 39);
+            this.buttonDisconnect.TabIndex = 2;
+            this.buttonDisconnect.Text = "Bağlantı Kes";
+            this.buttonDisconnect.UseVisualStyleBackColor = false;
+            this.buttonDisconnect.Click += new System.EventHandler(this.buttonDisconnect_Click);
+            // 
+            // textBoxReceived
+            // 
+            this.textBoxReceived.Location = new System.Drawing.Point(190, 242);
+            this.textBoxReceived.Multiline = true;
+            this.textBoxReceived.Name = "textBoxReceived";
+            this.textBoxReceived.Size = new System.Drawing.Size(456, 147);
+            this.textBoxReceived.TabIndex = 3;
+            this.textBoxReceived.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBoxSend
+            // 
+            this.textBoxSend.Location = new System.Drawing.Point(190, 396);
+            this.textBoxSend.Name = "textBoxSend";
+            this.textBoxSend.Size = new System.Drawing.Size(320, 20);
+            this.textBoxSend.TabIndex = 4;
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.BackColor = System.Drawing.Color.Gold;
+            this.buttonSend.Location = new System.Drawing.Point(535, 392);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(111, 32);
+            this.buttonSend.TabIndex = 5;
+            this.buttonSend.Text = "Gönder";
+            this.buttonSend.UseVisualStyleBackColor = false;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Salmon;
+            this.ClientSize = new System.Drawing.Size(800, 509);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.textBoxSend);
+            this.Controls.Add(this.textBoxReceived);
+            this.Controls.Add(this.buttonDisconnect);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.comboBoxPort);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxPort;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button buttonDisconnect;
+        private System.Windows.Forms.TextBox textBoxReceived;
+        private System.Windows.Forms.TextBox textBoxSend;
+        private System.Windows.Forms.Button buttonSend;
+    }
+}
+
---
+++ SerialPortChat/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SerialPortChat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (var seriPort in SerialPort.GetPortNames())
+            {
+                comboBoxPort.Items.Add(seriPort);
+            }
+            comboBoxPort.SelectedIndex = 0; //sıralı dizilim istersek
+            buttonDisconnect.Enabled = false;
+        }
+
+
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            serialPort1.PortName = comboBoxPort.Text; //hani combobox verisi seçilirse, o seri port'a bağla
+            serialPort1.BaudRate = 9600;
+            serialPort1.Parity = Parity.Even;
+            serialPort1.StopBits = StopBits.One;
+            serialPort1.DataBits = 8;
+
+            try 
+            {
+                serialPort1.Open();
+            }
+            
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Seri Port bağlantısı yapılamadı \n Hata: {ex.Message}","Problem", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                //MessageBox.Show("Lütfen Düzeltin", "Hata Oldu");
+            }
+
+            if (serialPort1.IsOpen)
+            {
+                buttonConnect.Enabled = false;
+                buttonDisconnect.Enabled = false;
+                this.buttonConnect.BackColor = Color.Red;
+            }
+        }
+
+        private void buttonDisconnect_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+                buttonConnect.Enabled = true;
+                buttonDisconnect.Enabled = false;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            if(serialPort1.IsOpen)
+            {
+                serialPort1.Write(textBoxSend.Text);
+                textBoxSend.Clear();
+            }
+        }
+
+        public delegate void veriGoster(String s); //karşılıklı iletişim.
+
+        public void textBoxYaz(String s)
+        {
+            textBoxReceived.Text += s;
+        }
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string gelenVeri = serialPort1.ReadExisting(); //bu kod satırı örneğin Merhaba yazdık, Merhabayı string olarak alır.
+            textBoxReceived.Invoke(new veriGoster(textBoxYaz), gelenVeri); //?
+        }
+    }
+}
